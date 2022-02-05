5014dd099247c2632a1f332de8a8b7b6552e3cea
Thomas Lichtenstein <tlichten@users.noreply.github.com>
2022-02-05 15:03:04 +0100

    Update commit.js

--- .github/actions/commit-action/commit.js
+++ .github/actions/commit-action/commit.js
@@ -5,15 +5,8 @@ const {
 } = process.env;
 // parsing the owner and repo name from GITHUB_REPOSITORY
 const [owner, repo] = GITHUB_REPOSITORY.split('/');
-const repoInfo = { owner, repo };
-const branch = "master";
-const createBranch = true;
 
-console.log(repoInfo);
-console.log(GITHUB_TOKEN);
-
-
-const { Octokit } = require("@octokit/core");
+const { Octokit } = require("@octokit/rest");
 const {
   createOrUpdateTextFile,
   composeCreateOrUpdateTextFile,
@@ -26,8 +19,24 @@ const octokit = new MyOctokit({auth: GITHUB_TOKEN});
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
 const main = async () => {
   try {
+    
+    const issue = github.context.payload.issue
+    if (!issue) return
+
+    const [issue_id] = issue.id
+    const issue_raw = await octokit.rest.issues.get({
+      owner,
+      repo,
+      issue_id,
+      mediaType: {
+        format: "raw",
+      }
+    });
+    console.log(issue_raw);
+    
     const {
       updated,
       data
